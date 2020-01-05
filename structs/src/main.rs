47584898967235b4322928c4ad48ae690bb4286e
Maikel Chandika <mkdika@gmail.com>
2020-01-05 22:35:15 +0700

    Add structs example

---
+++ structs/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let user1 = User {
+        username: String::from("maikel"),
+        email: String::from("mkdika@gmail.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+    println!("username      : {}", user1.username);
+    println!("email         : {}", user1.email);
+    println!("sign in count : {}", user1.sign_in_count);
+    println!("active        : {}", user1.active);
+    println!("");
+
+    let user2 = create_user(
+        String::from("max@gmail.com"),
+        String::from("maxmax")
+        );
+    println!("user2 is {:?}", user2);
+}
+
+fn create_user(email: String, username: String) -> User {
+    User {
+        username: username,
+        email: email,
+        sign_in_count: 1,
+        active: true,
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
